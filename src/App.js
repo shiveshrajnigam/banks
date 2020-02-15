45a17db88ace146732197923038c5b416a1397d7
shiveshrajnigam <33284557+shiveshrajnigam@users.noreply.github.com>
2020-02-15 20:51:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import classes from "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <header className={classes.AppHeader}>
+          <h1 className={classes.AppTitle}>Welcome to React</h1>
+        </header>
+        <p className={classes.AppIntro}>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
