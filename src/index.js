e76a0fb2bb8f7cbcefd661411fc5037f36c5a8ba
shiveshrajnigam <33284557+shiveshrajnigam@users.noreply.github.com>
2020-02-23 20:03:32 +0530

    added 404 route

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
 const app = (
-  <BrowserRouter basename="/app">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
