3fcfa3a413a56644de3695a38d3fcc8ac245f58a
shiveshrajnigam <33284557+shiveshrajnigam@users.noreply.github.com>
2020-02-23 19:52:29 +0530

    removed hashRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
 const app = (
-  <HashRouter>
+  <BrowserRouter basename="/app">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
